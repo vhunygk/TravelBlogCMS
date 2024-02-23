73279e39fc429aa3ce82a2b864ae378fd05b0a1a
VHUNYGK <amruta.pednekar@volkswagen.co.in>
2024-02-23 12:55:17 +0530

    Creation of apps
    Created backend and UI app

---
+++ travelblogcms-backend/src/test/java/com/example/travelblogcms/TravelblogcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelblogcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelblogcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
